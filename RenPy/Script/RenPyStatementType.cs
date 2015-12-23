50a2d0e58b9fc41c151c6f47dde5c37faeb928c0
Ava Pek <exodrifter@gmail.com>
2015-12-22 18:21:32 -0700

    Remove old Raconteur library

--- RenPy/Script/RenPyStatementType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DPek.Raconteur.RenPy.Script
-{
-	public enum RenPyStatementType {
-		BG, CALL, CHARACTER, COMMENT, ELIF, ELSE, HIDE, IF, IMAGE, INIT, JUMP,
-		LABEL, LINE, MENU, MENU_CHOICE, PASS, PAUSE, PLAY, QUEUE, RETURN, SAY,
-		SCENE, SHOW, STOP, VARIABLE, WHILE, WINDOW
-	};
-}
